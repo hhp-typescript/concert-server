9330276e1abe9eb999062d31c84ee398176d978a
leehyebin <970905hyebin@gmail.com>
2024-10-18 10:36:39 +0900

    chore: 스웨거 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('콘서트 티켓팅 API')
+    .setDescription('콘서트 티켓팅 API')
+    .setVersion('1.0')
+    .addTag('example')
+    .addBearerAuth(
+      { type: 'http', scheme: 'Bearer', bearerFormat: 'JWT' },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
