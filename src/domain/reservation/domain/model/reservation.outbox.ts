86b9be70d410fe03d781e17fc0e0ce785de9356f
leehyebin <970905hyebin@gmail.com>
2024-12-29 03:13:15 +0900

    feat: outbox model 추가

---
+++ src/domain/reservation/domain/model/reservation.outbox.ts
@@ -0,0 +1,39 @@
+export class ReservationOutbox {
+  id: number;
+  transactionId: string;
+  eventType: string;
+  payload: any;
+  status: OutboxStatus;
+
+  constructor(props: {
+    id?: number;
+    transactionId?: string;
+    eventType: string;
+    payload: any;
+    status?: OutboxStatus;
+  }) {
+    this.id = props.id;
+    this.transactionId = props.transactionId;
+    this.eventType = props.eventType;
+    this.payload = props.payload;
+    this.status = props.status;
+  }
+  static createNew(
+    transactionId: string,
+    eventType: string,
+    payload: any,
+  ): ReservationOutbox {
+    return new ReservationOutbox({
+      transactionId,
+      eventType,
+      payload,
+      status: OutboxStatus.INIT,
+    });
+  }
+}
+
+export enum OutboxStatus {
+  INIT = 'INIT',
+  SUCCESS = 'SUCCESS',
+  FAIL = 'FAIL',
+}
